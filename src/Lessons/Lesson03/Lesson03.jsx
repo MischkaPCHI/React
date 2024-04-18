57e590889817e26dbbcb41d219ffb56b698d7bf0
MischkaPCHI <michellejung4002@gmail.com>
2024-04-18 13:56:01 +0200

    redact files

---
+++ src/Lessons/Lesson03/Lesson03.jsx
@@ -0,0 +1,32 @@
+import "./styles.css"
+import ClassWorkProfileCards from "../../components/ClassWorkProfileCards/ClassWorkProfileCards"
+import Button from "../../components/Button/Button";
+import AnimalCard from "../../components/AnimalCard/AnimalCard";
+import { lionData, zebraData, profileData } from "./data";
+
+const normalizeName = (name) => {
+    let newName = name.toUpperCase();
+    return newName;
+}
+
+const Lesson03 = () => {
+    return (
+        <div className="profile-container">
+            {
+                profileData.map((link, i) => <ClassWorkProfileCards key={i} url={link.avatarUrl} name={link.name} 
+                job={link.job} hobby={link.hobby} />)
+            }
+
+            <AnimalCard animalData={lionData} animalName={normalizeName(lionData.name)} > 
+                <p>Ляяяяя</p>
+                <p>Заберите меня от сюда!!!</p>
+            </AnimalCard>
+            <AnimalCard animalData={zebraData} animalName={normalizeName(zebraData.name)} />
+
+            <Button buttonName="Send" isPrimary />
+            <Button buttonName="Confirm" />
+        </div>
+    )
+}
+
+export default Lesson03;
---
+++ src/Lessons/Lesson03/data.js
@@ -0,0 +1,36 @@
+let lionData = {
+    name: "Alex",
+    species: "Lion",
+    image:
+      "https://img.freepik.com/free-photo/cute-lion-nature-outdoors_23-2150813253.jpg?t=st=1713338293~exp=1713341893~hmac=1439b73aedcfad1324b607543807963d7c5774877d4b0043b2cb55bd0bca565e&w=740",
+};
+
+let zebraData = {
+    name: "Marty",
+    species: "Zebra",
+    image:
+      "https://img.freepik.com/premium-photo/meet-adorable-3d-baby-zebra-with-big-expressive-eyes-that-will-melt-your-heart-this-cartoon-cr_1032785-4269.jpg?w=740",
+};
+
+const profileData = [
+    {
+        avatarUrl: "https://cdn.kanobu.ru/editor/images/20/5d78da61-6c07-4088-babc-a00f9ca4f2b2.webp",
+        name: "Нёвиллет",
+        job: "Судья",
+        hobby: "Чтение"
+    },
+    {
+        avatarUrl: "https://pbs.twimg.com/media/EuQDGmtXIAEQnuR?format=jpg&name=900x900",
+        name: "Итачи Учиха",
+        job: "Ниндзя",
+        hobby: "Кулинария"
+    },
+    {
+        avatarUrl: "https://api.paygame.ru/media/blog/image/2023/01/14/word-image-195529-10_1.jpg",
+        name: "Аль Хайтам",
+        job: "Секретарь",
+        hobby: "Чтение"
+    }
+];
+
+export {lionData, zebraData, profileData};
